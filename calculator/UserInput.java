3e5957636648e02b378a1b513e5bf973b3c0b0d0
Arav Saxena <aravsaxena884@gmail.com>
2025-02-16 12:45:02 +0530

    Update UserInput.java

--- calculator/UserInput.java
+++ calculator/UserInput.java
@@ -1,17 +1,14 @@
-//userinput.java
-import java.util.*;
-class UserInput{
-	int[] UserInput(){
-		int[] numbers = new int[2];
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the First Number");
-		numbers[0] = sc.nextInt();
-		System.out.println("Enter the Second Number");
-		numbers[1] = sc.nextInt();
-		return numbers;//java cannot return multiple var
-//so we use array.
-		
-	}
-
+import java.util.Scanner;
+class Input {
+int[] input() {
+Scanner scan = new Scanner(System.in);
+System.out.print("Enter 1st number: ");
+int num1 = scan.nextInt();
+System.out.print("Enter 2nd number: ");
+int num2 = scan.nextInt();
+int[] numbers = new int[2];
+numbers[0] = num1;
+numbers[1] = num2;
+return numbers;
 }
-
+}
