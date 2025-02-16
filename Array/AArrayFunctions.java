0acdb627e18157e3e631fa25e35265da7adb3185
Arav Saxena <aravsaxena884@gmail.com>
2025-02-16 12:40:24 +0530

    Update AArrayFunctions.java

--- Array/AArrayFunctions.java
+++ Array/AArrayFunctions.java
@@ -1,57 +1,62 @@
 import java.util.*;
-class AArrayFunctions{
-	public void display(ArrayList<Integer> array){
-		System.out.println("Array is:"+array);
 
-	}
-	public void display1(int idx,int tar_diff){
-		System.out.println("idx is:"+idx);
-		System.out.println("Min diff is:"+tar_diff);
-
-
-	}
-
-	public void evenOdd(int[] array){//Array List takes Datatype in object from it do not take primitive data type
-		ArrayList<Integer> Even = new ArrayList<Integer>();
-		ArrayList<Integer> Odd = new ArrayList<Integer>();
-		for(int i = 0;i<array.length;i++){
-			if(array[i] % 2 == 0){
-				Even.add(array[i]);
-			}
-			else{
-				Odd.add(array[i]);
-			}
-		}
-		display(Even);
-		display(Odd);
-		
-	}
-	public void diff(int[] array) {
-    		int tar_diff = Integer.MAX_VALUE; // Start with a large difference
-    		int i = 0;
-    		int j = array.length - 1;
-    		int idx1 = -1, idx2 = -1; // To store indices of the pair with smallest difference
-
-    	while (i < j) {
-        	int diff = Math.abs(array[i] - array[j]); // Compute absolute difference
-        	if (diff < tar_diff) {
-            	tar_diff = diff;
-            	idx1 = i;
-            	idx2 = j;
-        }
-
-        // Move pointers based on values to try to minimize the difference
-        if (array[i] < array[j]) {
-            i++; // Move the left pointer forward
-        } else {
-            j--; // Move the right pointer backward
-        }
+class ArrayFunctions {
+    void display(ArrayList<Integer> array) {
+        System.out.println("Array is: " + array);
     }
 
-    display1(idx1, idx2, tar_diff); // Call display function with updated parameters
-}
-	
+    void evenOdd(int[] array) {
+        ArrayList<Integer> even = new ArrayList<>();
+        ArrayList<Integer> odd = new ArrayList<>();
 
+        for (int num : array) {
+            if (num % 2 == 0) {
+                even.add(num);
+            } else {
+                odd.add(num);
+            }
+        }
 
+        System.out.println("Even numbers: " + even);
+        System.out.println("Odd numbers: " + odd);
+    }
 
+    int mindiff(int[] array) {
+        if (array.length < 2) {
+            System.out.println("Array must have at least two elements.");
+            return -1;
+        }
+
+        int mindiff = Integer.MAX_VALUE;
+        int mindiffindex = 0;
+
+        for (int i = 0; i < array.length - 1; i++) {
+            int diff = Math.abs(array[i] - array[i + 1]); // Fixed absolute difference
+            if (diff < mindiff) {
+                mindiff = diff;
+                mindiffindex = i;
+            }
+        }
+
+        System.out.println("Minimum difference is: " + mindiff);
+        return mindiffindex;
+    }
+
+    // Function to convert array to ArrayList
+    void arrayToArrayList(int[] array) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        for (int num : array) {
+            arrayList.add(num);
+        }
+        System.out.println("Converted to ArrayList: " + arrayList);
+    }
+
+    // Function to convert ArrayList to array
+    void arrayListToArray(ArrayList<Integer> arrayList) {
+        int[] array = new int[arrayList.size()];
+        for (int i = 0; i < arrayList.size(); i++) {
+            array[i] = arrayList.get(i);
+        }
+        System.out.println("Converted back to array: " + Arrays.toString(array));
+    }
 }
