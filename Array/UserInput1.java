2afdb627815c95e02b049ec1ebe4a1a6d1b36c20
Arav Saxena <aravsaxena884@gmail.com>
2025-02-16 12:41:15 +0530

    Update UserInput1.java

--- Array/UserInput1.java
+++ Array/UserInput1.java
@@ -1,15 +1,24 @@
-//UserInput.java
 import java.util.*;
-class UserInput1{
-	int[] arrayInput(){
-		int[] array = new int[5];
-		Scanner Sc = new Scanner(System.in);
-		System.out.println("Enter 5 numbers:");
-		for(int i = 0;i<5;i++){
-			array[i] = Sc.nextInt();
-		}
-		return array;
-	}
 
+class Main {
+    public static void main(String args[]) {
+        UserInput ui = new UserInput();
+        ArrayFunctions functions = new ArrayFunctions();
 
+        int[] array = ui.arrayInput();
+
+        // 1. Separate even and odd numbers
+        functions.evenOdd(array);
+
+        // 2. Find the index of neighboring numbers with the smallest difference
+        int index = functions.mindiff(array);
+        System.out.println("Index with minimum difference: " + index);
+
+        // 3. Convert array to ArrayList
+        functions.arrayToArrayList(array);
+
+        // 4. Convert ArrayList back to array
+        ArrayList<Integer> arrayList = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5)); // Example list
+        functions.arrayListToArray(arrayList);
+    }
 }
