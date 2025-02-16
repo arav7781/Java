ad055ff1ded3781be7ebdf7f6916978ccfdf984e
Arav Saxena <aravsaxena884@gmail.com>
2025-02-16 15:56:33 +0530

    Update Calculator.java

--- calculator/Calculator.java
+++ calculator/Calculator.java
@@ -1,23 +1,77 @@
-//Calculator.java
-class Calculator{
-	int sum(){
-		UserInput ui = new UserInput();
-		int[] numbers = ui.UserInput();
-		int sum = numbers[0] + numbers[1];
-		return sum;
-	}
-	
-	int division(){
-		UserInput ui = new UserInput();
-		int[] numbers = ui.UserInput();
-		int sum = numbers[0]/numbers[1];
-		return sum;
-	}
-	int product(){
-		UserInput ui = new UserInput();
-		int[] numbers = ui.UserInput();
-		int sum = numbers[0]*numbers[1];
-		return sum;
-	}
-	
-}
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+class Calculator {
+
+    int sum() {
+        UserInput ui = new UserInput();
+        int[] numbers = ui.getUserInput();
+        return numbers[0] + numbers[1];
+    }
+
+    int subtraction() {
+        UserInput ui = new UserInput();
+        int[] numbers = ui.getUserInput();
+        return numbers[0] - numbers[1];
+    }
+
+    int product() {
+        UserInput ui = new UserInput();
+        int[] numbers = ui.getUserInput();
+        return numbers[0] * numbers[1];
+    }
+
+    int division() {
+        UserInput ui = new UserInput();
+        int[] numbers = ui.getUserInput();
+        if (numbers[1] == 0) {
+            System.out.println("Error: Division by zero is not allowed.");
+            return 0;
+        }
+        return numbers[0] / numbers[1];
+    }
+
+    double squareRoot() {
+        UserInput ui = new UserInput();
+        int[] numbers = ui.getUserInput();
+        return Math.sqrt(numbers[0]);
+    }
+
+    int fibonacci() {
+        UserInput ui = new UserInput();
+        int[] numbers = ui.getUserInput();
+        return fibonacciHelper(numbers[0]);
+    }
+
+    private int fibonacciHelper(int n) {
+        if (n <= 1) return n;
+        return fibonacciHelper(n - 1) + fibonacciHelper(n - 2);
+    }
+
+    double mean() {
+        UserInput ui = new UserInput();
+        int[] numbers = ui.getCustomArray();
+        return Arrays.stream(numbers).average().orElse(Double.NaN);
+    }
+
+    int mode() {
+        UserInput ui = new UserInput();
+        int[] numbers = ui.getCustomArray();
+        Map<Integer, Integer> frequencyMap = new HashMap<>();
+
+        for (int num : numbers) {
+            frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
+        }
+
+        int mode = numbers[0], maxCount = 0;
+        for (Map.Entry<Integer, Integer> entry : frequencyMap.entrySet()) {
+            if (entry.getValue() > maxCount) {
+                maxCount = entry.getValue();
+                mode = entry.getKey();
+            }
+        }
+
+        return mode;
+    }
