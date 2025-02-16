bb3277aeb042568a6951e134744ab36b71ae0377
Arav Saxena <aravsaxena884@gmail.com>
2025-02-16 12:54:02 +0530

    Update UserInput1.java

--- Array/UserInput1.java
+++ Array/UserInput1.java
@@ -1,16 +1,2 @@
 import java.util.*;
 
-class UserInput {
-    int[] arrayInput() {
-        int[] array = new int[5];
-        Scanner scan = new Scanner(System.in);
-
-        System.out.println("Enter 5 numbers:");
-
-        for (int i = 0; i < 5; i++) {
-            array[i] = scan.nextInt();
-        }
-
-        return array;
-    }
-}
