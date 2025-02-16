0e10f9fd860d6286c5691637bff39d7545e81e13
Arav Saxena <aravsaxena884@gmail.com>
2025-02-16 12:55:42 +0530

    Update Calculator.java

--- calculator/Calculator.java
+++ calculator/Calculator.java
@@ -7,17 +7,6 @@ class Calculator{
 		return sum;
 	}
 	
-	int division(){
-		UserInput ui = new UserInput();
-		int[] numbers = ui.UserInput();
-		int sum = numbers[0]/numbers[1];
-		return sum;
-	}
-	int product(){
-		UserInput ui = new UserInput();
-		int[] numbers = ui.UserInput();
-		int sum = numbers[0]*numbers[1];
-		return sum;
-	}
+	
 	
 }
