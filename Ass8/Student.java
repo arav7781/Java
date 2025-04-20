69026e73f096ff665c596eaf31219de2aae03546
Arav Saxena <aravsaxena884@gmail.com>
2025-04-20 17:48:10 +0530

    Update Student.java

--- Ass8/Student.java
+++ Ass8/Student.java
@@ -1,1 +1,16 @@
-
+// Student.java import
+java.time.LocalDate;
+public class Student {
+private String prn;
+private String name;
+private LocalDate dob;
+private double marks;
+ public Student(String prn, String name, LocalDate dob, double
+marks) { this.prn = prn; this.name = name;
+this.dob = dob; this.marks = marks;
+ }
+ public String getPrn() { return prn; }
+public String getName() { return name; }
+public LocalDate getDob() { return dob; }
+public double getMarks() { return marks; }
+ public void setName(String name) { this.name = name; }
