cfa6622550179e1f21b312065cf3f9554400de61
Arav Saxena <aravsaxena884@gmail.com>
2025-04-20 17:54:02 +0530

    Update main.java

--- Ass10/main.java
+++ Ass10/main.java
@@ -1,5 +1,25 @@
-package Ass10;
+public class JavaThreadsDemo {
+    // Thread class using extends Thread     static class MyThread1 extends Thread {
+        @Override         public void run() {
+            // Printing numbers from 1 to 5
+            System.out.println("MyThread1 is printing numbers 1 to 5:");             for (int i = 1; i <= 5; i++) {
+                System.out.println("Thread-1: " + i);                 try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    System.out.println("Thread-1 Interrupted");
+                }
+            }
+        }
+    }
+    // Thread class implementing Runnable     static class MyThread2 implements Runnable {
+        @Override         public void run() {
+            // Printing characters from A to E
+            System.out.println("MyThread2 is printing characters A to E:");             for (char c = 'A'; c <= 'E'; c++) {                 System.out.println("Thread-2: " + c);                 try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    System.out.println("Thread-2 Interrupted");
+                }
+            }
+        }
+    }
 
-public class main {
-    
-}
