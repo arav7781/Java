c9719416537750f8bb7ffb3856d532a5bbb1897c
Arav Saxena <aravsaxena884@gmail.com>
2025-04-20 17:54:23 +0530

    Update main.java

--- Ass10/main.java
+++ Ass10/main.java
@@ -23,3 +23,18 @@ public class JavaThreadsDemo {
         }
     }
 
+// Thread class implementing Runnable     static class MyThread2 implements Runnable {
+        @Override         public void run() {
+            // Printing characters from A to E
+            System.out.println("MyThread2 is printing characters A to E:");             for (char c = 'A'; c <= 'E'; c++) {                 System.out.println("Thread-2: " + c);                 try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    System.out.println("Thread-2 Interrupted");
+                }
+            }
+        }
+    }
+    // Start thread using class that extends Thread     public static void startThreadOne() {         MyThread1 t1 = new MyThread1();         t1.start();
+    }
+    // Start thread using class that implements Runnable     public static void startThreadTwo() {         Thread t2 = new Thread(new MyThread2());         t2.start();
+    }
